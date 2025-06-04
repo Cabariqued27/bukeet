d1b8f6dd4eaf51b51742a02a5c83ae5d8eefbb55
Cabariqued27 <felipe27th@gmail.com>
2025-06-03 22:09:50 -0500

    First Deploy June

--- lib/settings/app_settings.dart
+++ lib/settings/app_settings.dart
@@ -3,7 +3,7 @@ import 'package:bukeet/models/services_settings.dart';
 
 class AppSetting {
   static late ServiceSetting servicesSettings;
-  static const String appVersion = '1.0.1';
+  static const String appVersion = '1.0.2';
   static const int validLegalAge = 10;
 
   static const int minAverageWindow = 2;
